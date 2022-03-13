58fc27c2c3077525b0381d06c180e3d8a58e08d3
rana.lubis <rana.lubis@gmail.com>
2022-03-13 11:52:47 +0700

    rework route

--- src/context.ts
+++ src/context.ts
@@ -1,16 +1,12 @@
 import { Request, Response } from "express";
-import {
-  map,
-  Observable,
-  of,
-  OperatorFunction,
-  Subject,
-  switchMap,
-} from "rxjs";
+import { Observable, of, Subject } from "rxjs";
 import { Stream } from "stream";
 import * as R from "ramda";
-
+import { Router } from "express";
 import { Logger } from "./logger";
+
+const logger = Logger.logger;
+const tokensPattern = new RegExp(/\:(.+)/, "g");
 
 export declare type IHttpParam = {
   from: "query" | "path" | "body";
@@ -77,46 +73,6 @@ export const streamResponse =
   (res: Response) =>
     source.pipe(res);
 
-const incoming = new Subject<IHttpIncoming>();
-
-export const inQueue$ = incoming.asObservable();
-export const handleIncoming = (context: IHttpIncoming) =>
-  incoming.next(context);
-
-const handleResponse =
-  (res: Response) => (source: Observable<IHttpResponse>) => {
-    source.subscribe({
-      next(handler) {
-        if (res.headersSent) return;
-
-        try {
-          handler(res);
-        } catch (e) {
-          Logger.logger.error(e);
-          res.status(500).send(e);
-        }
-      },
-
-      error(err) {
-        Logger.logger.error(err);
-        res.status(500).send(err);
-      },
-    });
-  };
-
-export const route = R.compose((fn: OperatorFunction<any, IHttpResponse>) => {
-  inQueue$.subscribe({
-    next({ ctx, res }) {
-      const handler = handleResponse(res);
-      handler(fn(of(ctx)));
-    },
-
-    error(err) {
-      Logger.logger.error(err);
-    },
-  });
-}, R.pipe);
-
 export interface Route {
   (context: Observable<IHttpContext>): Observable<any>;
 }
@@ -142,8 +98,100 @@ export const chunkFromStream = (source: Observable<any>) =>
     });
   });
 
-export const handleRoute = (req: Request, res: Response) => {
+const attach = (route: Route) => (req: Request, res: Response) => {
   const ctx = applyContext(req);
 
-  handleIncoming({ ctx, res });
+  route(of(ctx)).subscribe({
+    next(output) {
+      if (output instanceof Observable) {
+        // send as chunk
+        res.writeHead(200, {
+          "Content-Type": "text/plain; charset=utf-8",
+          "Transfer-Encoding": "chunked",
+        });
+
+        output.subscribe({
+          next(v) {
+            res.write(
+              Buffer.from(JSON.stringify(v), "utf-8").toString("base64") + "\n"
+            );
+          },
+          error(err) {
+            logger.error(err);
+            res.write("error");
+          },
+          complete() {
+            res.write(
+              Buffer.from(JSON.stringify("done"), "utf-8").toString("base64")
+            );
+            res.end();
+          },
+        });
+      } else res.status(200).send(output);
+    },
+
+    error(err) {
+      logger.error(err);
+      logger.debug(JSON.stringify(err, null, 4));
+
+      if (!res.headersSent)
+        res.status(500).send(`${err.message}, reason: \n\t\t${err.stack}`);
+    },
+  });
+};
+
+const sortRoutes = (a: string, b: string) => {
+  const aTokens = a.split("/");
+  const bTokens = b.split("/");
+
+  const result = (() => {
+    // if (aTokens.length != bTokens.length)
+    // return bTokens.length - aTokens.length;
+    for (let i = 0; i < aTokens.length; i++) {
+      if (aTokens[i] && !bTokens[i]) return -1;
+      else if (!aTokens[i] && bTokens[i]) return 1;
+
+      if (aTokens[i] !== bTokens[i]) {
+        if (tokensPattern.test(aTokens[i]) && !tokensPattern.test(bTokens[i]))
+          return 1;
+        else if (
+          !tokensPattern.test(aTokens[i]) &&
+          tokensPattern.test(bTokens[i])
+        )
+          return -1;
+        else if (
+          tokensPattern.test(aTokens[i]) &&
+          tokensPattern.test(bTokens[i])
+        )
+          return bTokens[i].length - aTokens[i].length;
+      }
+    }
+
+    return 0;
+  })();
+
+  // console.log(a, b, result);
+  return result;
+};
+
+const normalizePath = (routePath: string) => {
+  const pathTokens = routePath
+    .split("_")
+    .map((p) => (p.startsWith("$") ? ":" + p.substring(1) : p));
+  const [method, ...parts] = pathTokens;
+  return { routePath: "/" + parts.join("/"), method };
+};
+
+export const attachRouter = (routes: { [key: string]: Route }) => {
+  const router: Record<string, any> = Router();
+  const modules = Object.keys(routes)
+    .map((k) => ({
+      ...normalizePath(k),
+      route: routes[k],
+    }))
+    .sort((a, b) => sortRoutes(a.routePath, b.routePath));
+
+  for (let m of modules) router[m.method](m.routePath, attach(m.route));
+
+  return router;
 };
